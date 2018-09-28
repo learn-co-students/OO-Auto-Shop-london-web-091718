4cc25879cd928d5d331a8e05c958c2580bc0d98b
wimbleclaw <aisha305@hotmail.co.uk>
2018-09-28 08:25:45 +0100

    classes

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,11 +1,32 @@
 class Mechanic
 
+  @@all = []
+
   attr_reader :name, :specialty
 
-def initialize(name, specialty)
-  @name = name
-  @specialty = specialty
-end
+  def initialize(name, specialty)
+    @name = name
+    @specialty = specialty
+    @@all << self
+  end
 
+  def self.all
+    @@all
+  end
+
+  #ßGet a list of all cars that a mechanic services
+  def cars
+    Car.all.select {|car| car.mechanic == self}
+  end
+
+  #Get a list of all the car owners that go to a specific mechanic
+  def car_owners
+    cars.collect {|car| car.carowner}
+  end
+
+
+  def car_owners_names
+    car_owners.collect {|owner| owner.name}
+  end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,16 @@ def reload
   load 'config/environment.rb'
 end
 
+ryan = Carowner.new('Ryan')
+tinkerbell = Carowner.new('Tinkerbell')
+
+bob = Mechanic.new('Bob', 'antique')
+felix = Mechanic.new('Felix', 'exotic')
+
+bmw = Car.new('BMW', 'x', 'antique', tinkerbell, bob)
+merc = Car.new('merc', 'h', 'exotic', ryan, felix)
+
+
+
+
 binding.pry
